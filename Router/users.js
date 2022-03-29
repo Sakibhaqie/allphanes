3770781260ea10c69da687fb26ceaae0372fc075
sakibhaqie <imdadulhaque72@gmail.com>
2022-03-29 12:21:44 +0530

    mails

--- Router/users.js
+++ Router/users.js
@@ -35,39 +35,33 @@ const create = async (req, res, next) => {
             otp: hashOTP,
             otpExpTime: otpExp
         })
-
-        // "use strict";
+"use strict";
 const nodemailer = require("nodemailer");
 
 // async..await is not allowed in global scope, must use a wrapper
 async function main() {
   // Generate test SMTP service account from ethereal.email
   // Only needed if you don't have a real mail account for testing
-//   let testAccount = await nodemailer.createTestAccount();
+  let testAccount = await nodemailer.createTestAccount();
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com",
-    service: "hotmail",
+    host: "smtp.ethereal.email",
     port: 587,
     secure: false, // true for 465, false for other ports
-    tls: {
-        ciphers: "SSLv3",
-        rejectUnauthorized: false,
-      },
     auth: {
-      user: process.env.AUTH_EMAIL, // generated ethereal user
-      pass: process.env.AUTH_PASS, // generated ethereal password
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
     },
   });
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: '"SK" <imdadulhaque72@hotmail.com>', // sender address
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
     to: email, // list of receivers
     subject: "Hello ✔", // Subject line
     text: "Hello world?", // plain text body
-    html: "<h1>"+randotp+"</h1>", // html body
+    html: "<b>Hello world?</b>", // html body
   });
 
   console.log("Message sent: %s", info.messageId);
