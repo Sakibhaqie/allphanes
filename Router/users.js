0c572a5370466df621178681d9a981c3eedd046e
sakibhaqie <imdadulhaque72@gmail.com>
2022-03-29 15:55:18 +0530

    mail with nodemailer

--- Router/users.js
+++ Router/users.js
@@ -57,11 +57,11 @@ async function main() {
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    from: '"Fred Foo 👻" '+ testAccount.user, // sender address
     to: email, // list of receivers
     subject: "Hello ✔", // Subject line
     text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
+    html: "<h1>"+ randotp +"</h1>", // html body
   });
 
   console.log("Message sent: %s", info.messageId);
